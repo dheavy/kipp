48349100c839a73bb348e6d2ff6dccf6e803c0d0
Davy Peter Braun <hello@davybraun.com>
2015-03-15 16:16:13 +0100

    Add detection of Vimeo.

--- bookmarklet/kipp/kipp.js
+++ bookmarklet/kipp/kipp.js
@@ -71,6 +71,8 @@
   KIPP.findPatterns = function () {
     if (!KIPP.patterns) return;
 
+    var index = 0;
+
     $.each(KIPP.patterns, function iter(i, pattern) {
       // Check URL.
       if (window.location.href.indexOf(pattern.urlPattern) != -1) {
@@ -79,8 +81,7 @@
       }
 
       // Check DOM.
-      var $search = $(pattern.tag),
-          index = 0;
+      var $search = $(pattern.tag);
 
       if ($search.length > 0) {
         $.each($search, function iter(i, elm) {
@@ -163,6 +164,18 @@
       generator: function (embedUrl) {
         var id = embedUrl.substring(embedUrl.lastIndexOf('/') + 1);
         return 'https://www.youtube.com/watch?v=' + id;
+      }
+    },
+    {
+      name: 'vimeo',
+      urlPattern: 'vimeo.com/',
+      tag: "iframe[src*='player.vimeo.com/video']",
+      generator: function (embedUrl) {
+        var matches = /(\/)(\d+)/.exec(embedUrl), id;
+        if (matches[2]) {
+          id = matches[2];
+          return 'https://vimeo.com/' + id;
+        }
       }
     }
   ]
